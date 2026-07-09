3f05fc12c1bae515b9b84aa311d1c888b1021a31
shreyash Yadav <shreyashy49@gmail.com>
2026-07-09 15:34:24 +0530

    my pet section created

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -6,6 +6,10 @@ import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
 import VerifyOTP from "../pages/auth/VerifyOTP";
 import DashboardHome from "../pages/dashboard/DashboardHome";
+import MyPets from "../pages/pets/MyPets/MyPets";
+import AddPet from "../pages/pets/AddPet/AddPet";
+import PetDetails from "../pages/pets/PetDetails/PetDetails";
+import EditPet from "../pages/pets/EditPet/EditPet";
 
 const AppRoutes = () => {
   return (
@@ -17,6 +21,14 @@ const AppRoutes = () => {
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/verify" element={<VerifyOTP />} />
       <Route path="/dashboard" element={<DashboardHome />} />
+      
+      {/* Pet Routes */}
+      <Route path="/pets" element={<MyPets />} />
+      <Route path="/pets/add" element={<AddPet />} />
+      <Route path="/pets/new" element={<AddPet />} />
+      <Route path="/pets/:id" element={<PetDetails />} />
+      <Route path="/pets/:id/edit" element={<EditPet />} />
+
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
